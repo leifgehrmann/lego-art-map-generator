266e2b0577b86b6cbe97e305b3d2905a111dbd38
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-10-13 21:45:09 +0100

    Refactored wgs84_to_lego transformer so StretchBands can be reused elsewhere

--- map_generator/lego_projection_transformer_builder.py
+++ map_generator/lego_projection_transformer_builder.py
@@ -1,9 +1,19 @@
-from typing import Tuple, Callable
+from math import isclose
+
+from typing import Tuple, Callable, TypedDict, List
 
 from map_engraver.canvas.canvas_unit import CanvasUnit
 
 
+class StretchRange(TypedDict):
+    latitude_range_start: float
+    latitude_range_stop: float
+    canvas_range_start: float
+    canvas_range_stop: float
+
+
 class LegoProjectionTransformerBuilder:
+    stretch_bands: List[StretchRange]
 
     def __init__(
             self,
@@ -12,6 +22,38 @@ class LegoProjectionTransformerBuilder:
     ):
         self.canvas_width = canvas_width
         self.canvas_height = canvas_height
+        self.stretch_bands = [
+            {
+                'latitude_range_start': -90.0,
+                'latitude_range_stop': -83.0,
+                'canvas_range_start': CanvasUnit.from_px(80 + 1).pt,
+                'canvas_range_stop': CanvasUnit.from_px(80).pt,
+            },
+            {
+                'latitude_range_start': -83.0,
+                'latitude_range_stop': -60.0,
+                'canvas_range_start': CanvasUnit.from_px(80).pt,
+                'canvas_range_stop': CanvasUnit.from_px(70).pt,
+            },
+            {
+                'latitude_range_start': -60.0,
+                'latitude_range_stop': -57.0,
+                'canvas_range_start': CanvasUnit.from_px(70).pt,
+                'canvas_range_stop': CanvasUnit.from_px(67).pt,
+            },
+            {
+                'latitude_range_start': -57.0,
+                'latitude_range_stop': 86.0,
+                'canvas_range_start': CanvasUnit.from_px(67).pt,
+                'canvas_range_stop': CanvasUnit.from_px(4).pt,
+            },
+            {
+                'latitude_range_start': 86.0,
+                'latitude_range_stop': 90.0,
+                'canvas_range_start': CanvasUnit.from_px(4).pt,
+                'canvas_range_stop': CanvasUnit.from_px(0).pt,
+            }
+        ]
 
     def build_wgs84_to_lego(
             self
@@ -28,40 +70,36 @@ class LegoProjectionTransformerBuilder:
             x_offset = CanvasUnit.from_px(-4)
             x_canvas = x_percentage * self.canvas_width.pt + x_offset.pt
 
+            min_latitude_range = None
+            max_latitude_range = None
+            min_y_range = None
+            max_y_range = None
+
             # The latitude mappings below assume that the canvas height is
             # 80px.
-            if latitude < -83:
-                min_latitude_range = -90
-                max_latitude_range = -83
-                # Add 1 to ensure the transformed polygon doesn't
-                # self-intersect.
-                min_y_range = CanvasUnit.from_px(80 + 1).pt
-                max_y_range = CanvasUnit.from_px(80).pt
-            elif latitude < -60:
-                min_latitude_range = -83
-                max_latitude_range = -60
-                min_y_range = CanvasUnit.from_px(80).pt
-                max_y_range = CanvasUnit.from_px(70).pt
-            elif latitude < -57:
-                min_latitude_range = -60
-                max_latitude_range = -57
-                min_y_range = CanvasUnit.from_px(70).pt
-                max_y_range = CanvasUnit.from_px(67).pt
-            elif latitude < 86:
-                min_latitude_range = -57
-                max_latitude_range = 86
-                min_y_range = CanvasUnit.from_px(67).pt
-                max_y_range = CanvasUnit.from_px(4).pt
-            elif latitude < 86:
-                min_latitude_range = 86
-                max_latitude_range = 86
-                min_y_range = CanvasUnit.from_px(4).pt
-                max_y_range = CanvasUnit.from_px(4).pt
-            else:
-                min_latitude_range = 86
-                max_latitude_range = 90
-                min_y_range = CanvasUnit.from_px(4).pt
-                max_y_range = CanvasUnit.from_px(0).pt
+            for stretch_band in self.stretch_bands:
+                if ((
+                        stretch_band['latitude_range_start'] <= latitude or
+                        isclose(stretch_band['latitude_range_start'], latitude)
+                ) and
+                        latitude < stretch_band['latitude_range_stop']
+                ):
+                    min_latitude_range = stretch_band['latitude_range_start']
+                    max_latitude_range = stretch_band['latitude_range_stop']
+                    min_y_range = stretch_band['canvas_range_start']
+                    max_y_range = stretch_band['canvas_range_stop']
+                    break
+
+            if (
+                    min_latitude_range is None or
+                    max_latitude_range is None or
+                    min_y_range is None or
+                    max_y_range is None
+            ):
+                raise Exception(
+                    'Coordinate (%f, %f) could not be projected' %
+                    (longitude, latitude)
+                )
 
             y_percentage = (latitude - min_latitude_range) / \
                            (max_latitude_range - min_latitude_range)
