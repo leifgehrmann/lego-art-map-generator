20308ef5b582684521d4987713840dca4b022a92
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-11-01 22:24:44 +0000

    Improve analysis scripts to output real CSV, not fake stdout CSV

--- map_analysis/compare_tiles_to_sea_depth.py
+++ map_analysis/compare_tiles_to_sea_depth.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+from typing import Dict
 
 import click
+import csv
 from PIL import Image
 
 
@@ -22,6 +24,39 @@ def get_tile_name_alias(tile_name: str) -> str:
     return tile_name
 
 
+def save_csv(
+        tile_count: Dict[str, Dict[int, int]],
+        tile_total_count: Dict[int, int],
+        dst_path: Path
+) -> None:
+    with open(dst_path.as_posix(), 'w', newline='') as dst_file:
+        writer = csv.writer(dst_file)
+
+        # Print out headers
+        headers = ['tile']
+        headers.extend(range(2 ** 8))
+        writer.writerow(headers)
+
+        # Print out full totals
+        row = ['Total']
+        for sea_value in range(2 ** 8):
+            if sea_value not in tile_total_count:
+                row.append(0)
+            else:
+                row.append(tile_total_count[sea_value])
+        writer.writerow(row)
+
+        # Print out the totals, sorted alphabetically
+        for tile_name in [i for i in sorted(tile_count.keys())]:
+            row = [tile_name]
+            for sea_value in range(2 ** 8):
+                if sea_value not in tile_count[tile_name]:
+                    row.append(0)
+                else:
+                    row.append(tile_count[tile_name][sea_value])
+            writer.writerow(row)
+
+
 @click.command()
 @click.argument(
     'tile',
@@ -33,7 +68,17 @@ def get_tile_name_alias(tile_name: str) -> str:
     nargs=1,
     type=click.Path(exists=True)
 )
-def count(tile, sea):
+@click.argument(
+    'dst',
+    nargs=1,
+    type=click.Path(exists=False)
+)
+@click.option(
+    "--image/--csv",
+    default=False,
+    help='Output a CSV or an image'
+)
+def count(tile, sea, dst, image):
     tile_count = {}
     tile_total_count = {}
 
@@ -46,7 +91,7 @@ def count(tile, sea):
     for y in range(tile_image.height):
         for x in range(tile_image.width):
             tile_name = "%d,%d,%d" % tile_image.getpixel((x, y))
-            sea_value = str(sea_image.getpixel((x, y))[0])
+            sea_value = sea_image.getpixel((x, y))[0]
             tile_name = get_tile_name_alias(tile_name)
             if tile_name not in tile_count:
                 tile_count[tile_name] = {}
@@ -58,27 +103,14 @@ def count(tile, sea):
                 tile_total_count[sea_value] = 0
             tile_total_count[sea_value] += 1
 
-    # Print out headers
-    headers = ['tile']
-    headers.extend(range(2 ** 8))
-    print(str(headers)[1:-1])
-    # Print out full totals
-    row = ['Total']
-    for sea_value in range(2 ** 8):
-        if str(sea_value) not in tile_total_count:
-            row.append(0)
-        else:
-            row.append(tile_total_count[str(sea_value)])
-    print(str(row)[1:-1])
-    # Print out the totals, sorted alphabetically
-    for tile_name in [i for i in sorted(tile_count.keys())]:
-        row = [tile_name]
-        for sea_value in range(2 ** 8):
-            if str(sea_value) not in tile_count[tile_name]:
-                row.append(0)
-            else:
-                row.append(tile_count[tile_name][str(sea_value)])
-        print(str(row)[1:-1])
+    if image:
+        pass
+    else:
+        save_csv(
+            tile_count,
+            tile_total_count,
+            Path(dst)
+        )
 
 
 if __name__ == '__main__':
